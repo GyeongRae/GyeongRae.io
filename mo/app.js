825d54bb8c373d3f7ea3ddf38ac0aba499bd4668
이경래- <rudfo4206@naver.com>
2021-07-14 14:51:08 +0000

    html 에 js 수정

--- mo/app.js
+++ mo/app.js
@@ -1,9 +1,22 @@
-const age = 96;
-function caculatorKrAge(ageOF){
-	age + 2;
-	}
+const age = parseInt(prompt("How old are you"));
 
 
-const krAge = caculatorKrAge(dageOF);
-
-console.log(Kr.age);
+if(isNaN(age) || age < 0){
+	console.log("please write a real positve number");
+} else if (age< 18) {
+	console.log("you are too young.");
+} else if (age >= 18 && age <= 50){
+	console.log("you can drink")
+}
+  else if (age > 50 && age <= 80){
+	console.log("you should exercise");
+}
+   else if (age ===100){
+	console.log("wow you are wise");
+}
+	else if (age > 80){
+   console.log("you can do whatever you want");
+}
+  
+if((a&&b) || (c &&d))
+	///3.0
