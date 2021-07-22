e51d68fda4bf82a625a6b3b732d378eca1e109b6
이경래- <rudfo4206@naver.com>
2021-07-22 14:13:32 +0000

    evnet study

--- mo/app.js
+++ mo/app.js
@@ -1,3 +1,36 @@
-const title= document.querySelector(".hello h1");
+const h1= document.querySelector(".hello:first-child h1");
 
-console.log(title);
+
+function handleTitleClick(){
+	h1.style.color = "blue";
+}
+
+function handMouseEnter(){
+	h1.innerText = "Mouse is here";
+}
+
+function handMouseLeave(){
+	h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+	document.body.style.backgroundColor = "tomato";
+}
+function handWindowCopy(){
+	alert("copier");	
+}
+
+function handWindowOffine(){
+	alert("sos no wifi");
+}
+function handWindowOnline(){
+	alert("wifi is on");
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handMouseEnter);
+h1.addEventListener("mouseleave",handMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handWindowCopy);
+window.addEventListener("offline", handWindowOffine);
+window.addEventListener("online", handWindowOnline);
