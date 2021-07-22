4e70721d7cf317c292872096ab2c6cf0ec4853bd
이경래- <rudfo4206@naver.com>
2021-07-22 14:36:19 +0000

    javascript revise css

--- mo/app.js
+++ mo/app.js
@@ -1,36 +1,16 @@
-const h1= document.querySelector(".hello:first-child h1");
+const h1= document.querySelector("div.hello:first-child h1");
 
 
 function handleTitleClick(){
-	h1.style.color = "blue";
+	const currentColor = h1.style.color;
+	let newColor;
+	if(currentColor === "blue"){
+		newColor = "tomato";
+	} 
+	else{
+		newColor = "blue";	
+	}
+	h1.style.color = newColor;
 }
 
-function handMouseEnter(){
-	h1.innerText = "Mouse is here";
-}
-
-function handMouseLeave(){
-	h1.innerText = "Mouse is gone";
-}
-
-function handleWindowResize(){
-	document.body.style.backgroundColor = "tomato";
-}
-function handWindowCopy(){
-	alert("copier");	
-}
-
-function handWindowOffine(){
-	alert("sos no wifi");
-}
-function handWindowOnline(){
-	alert("wifi is on");
-}
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handMouseEnter);
-h1.addEventListener("mouseleave",handMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handWindowCopy);
-window.addEventListener("offline", handWindowOffine);
-window.addEventListener("online", handWindowOnline);
