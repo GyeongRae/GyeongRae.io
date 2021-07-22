92c67bc38028bf1d4c3af9d015bd64d534443512
이경래- <rudfo4206@naver.com>
2021-07-22 14:53:49 +0000

    css

--- mo/app.js
+++ mo/app.js
@@ -1,16 +1,13 @@
-const h1= document.querySelector("div.hello:first-child h1");
-
+const h1= document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
-	const currentColor = h1.style.color;
-	let newColor;
-	if(currentColor === "blue"){
-		newColor = "tomato";
-	} 
-	else{
-		newColor = "blue";	
+	
+	if(h1.className === "active"){
+		h1.className = "";
 	}
-	h1.style.color = newColor;
+	else{
+		h1.className = "active";
+	}
 }
 
 h1.addEventListener("click", handleTitleClick);
